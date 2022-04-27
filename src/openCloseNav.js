5b0228609c5317116e47458c0d90b66a29f1388d
caliLai <calilai2018@gmail.com>
2022-04-27 05:15:31 -0700

    fixed footer and nav

---
+++ src/openCloseNav.js
@@ -0,0 +1,7 @@
+function openNav() {
+	document.getElementsByTagName("nav")[0].style.display = "flex";
+}
+  
+function closeNav() {
+	document.getElementsByTagName("nav")[0].style.display = "none";
+}
